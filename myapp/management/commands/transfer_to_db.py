72c0eb6c0cceb738428baca1d7b9e38999a5259f
ME17FD <faddak.mehdi@gmail.com>
2024-08-10 22:58:30 +0100

    new command to transfer data from excel

---
+++ myapp/management/commands/transfer_to_db.py
@@ -0,0 +1,90 @@
+from sys import exception
+from django.core.management.base import BaseCommand
+import pandas as pd
+from myapp.models import person
+
+class Command(BaseCommand):
+    help = 'Transfer data from Excel to database'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+        "--delete_old",  # Optional argument
+        action="store_true",
+        default=False,  # Default to False
+        help="Flag to delete old data"
+        )
+        parser.add_argument(
+        "--excel_file",  # Optional argument for the Excel file
+        type=str,  # Expecting a string (file path)
+        default="ex.xlsx",  # Default file name/path if not provided
+        help="Path to the Excel file"
+        )
+
+
+    def handle(self, *args, **options):
+        self.stdout.write('Starting data transfer...')
+
+        
+        if options.get('delete_old'):
+            delete_list= person.objects.all()
+            nb_dl = 0
+            for to_delete in delete_list:
+                to_delete.delete()
+                nb_dl +=1
+            print(f'{nb_dl} deleted from database')
+        
+        
+        # Load data from Excel into a DataFrame
+        excel_file = options.get('excel_file')
+        df = pd.read_excel(excel_file)
+
+        # Initialize a counter for errors
+        errors = 0
+        created_c =0
+        update_c = 0
+        duplicate_c=0
+        # Iterate through the rows and process data
+        for index, row in df.iterrows():
+            fname = row.get('Nom', '')
+            lname = row.get('Prenom', '')
+            cin = row.get('CIN', '')
+            cne = row.get('CNE', '')
+            info = row.get('INFO', '')
+            email = row.get('email', '')
+            
+            if not pd.isna(email) and '@' not in email:
+                email = ''
+
+            # Create or update the Person object
+            try:
+                person_obj, created = person.objects.get_or_create(
+                    fname=fname, lname=lname, cin=cin,
+                    defaults={
+                        'ismale': True,  # Default values for new objects
+                        'email': email,
+                        'phone': '',
+                        'cen': cne,
+                        'info': info,
+                        'bday': '',
+                    }
+                )
+                if not created:  # If the person already exists
+                    if info and (info not in person_obj.info):
+                        person_obj.info += '\n' + info
+                        person_obj.save()
+                        update_c +=1
+                    else:
+                        if info !='' : duplicate_c +=1
+
+                else:
+                    created_c +=1
+            except Exception as e:
+                self.stdout.write(f'Error processing row {index}: {e}')
+                errors += 1
+
+        # Output the number of errors encountered
+        self.stdout.write(f'Total number profiles created: {created_c}')
+        self.stdout.write(f'Total number profiles updated: {update_c}')
+        self.stdout.write(f'Total number duplicates found: {duplicate_c}')
+        self.stdout.write(f'Total number of errors: {errors}')
+        self.stdout.write("Data transferred to database successfully.")
